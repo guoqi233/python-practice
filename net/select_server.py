3436c12977d26b309affeb193ed87a6464b42c33
guoqi <415704688@qq.com>
2019-05-12 12:27:47 +0800

    多进程

--- net/select_server.py
+++ net/select_server.py
@@ -1,10 +1,13 @@
 import socket
 import select
 import sys
+import os
 import array
 import json
+from multiprocessing import Process
 
 SERVER_HOST = "localhost"
+RECV_MAX_SIZE = 4096
 
 
 class Server(object):
@@ -16,12 +19,23 @@ class Server(object):
         self.server.listen(backlog)
         self.outputs = []
 
+    def start(self):
+        pths = []
+        for i in range(4):
+            p = Process(target=self.run)
+            pths.append(p)
+            p.start()
+        for item in pths:
+            item.join()
+
     def run(self):
-        inputs = [self.server, sys.stdin]
+        print("pid is {0}".format(os.getpid()))
+        inputs = [self.server, ]
         running = True
         while running:
             try:
-                readable, writeable, exceptional = select.select(inputs, self.outputs, [])
+                readable, writeable, exceptional = select.select(
+                    inputs, self.outputs, [])
             except KeyboardInterrupt:
                 running = False
                 continue
@@ -39,21 +53,23 @@ class Server(object):
                         print("your input is:{}".format(result))
                     break
                 else:
-                    fp = array.array("u", " "*4096)
-                    sock.recv_into(fp, 4096)
-                    info = fp.tostring()
-                    if not info.strip():
+                    recv_data = sock.recv(RECV_MAX_SIZE)
+                    if not recv_data:
+                        print("leave")
                         inputs.remove(sock)
-                        break
-                    print(info.strip())
+                        continue
+                    print("pid {0} recv: {1}".format(
+                        os.getpid(), recv_data.decode("utf-8").strip()))
                     data = {
-                            "code": "Success",
-                        }
-                    sock.sendall(json.dumps(data))
+                        "code": "Success",
+                    }
+                    send_data = "{0}\n".format(json.dumps(data, indent=4))
+                    sock.sendall(send_data.encode())
         else:
             sys.stderr.write("\nserver is down\n")
 
 
 if __name__ == '__main__':
     server = Server(7457)
-    server.run()
+    # server.run()
+    server.start()
